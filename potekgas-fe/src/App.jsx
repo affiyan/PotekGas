4ccec448dc82f60ba2fc6c7d6076227c0092ac67
Daffa Abiyyu <daffaabiyyu04@gmail.com>
2024-04-23 07:40:35 +0700

    ppp

--- potekgas-fe/src/App.jsx
+++ potekgas-fe/src/App.jsx
@@ -155,22 +155,68 @@ function App() {
                 </div>
               </>
             ) : (
-              <div id="wrapper">
-                <HeaderComponent />
-                <div id="content-wrapper" className="d-flex flex-column">
-                  <NavbarComponent />
-                  {/* Main Content */}
-                  <div id="content" className="container-fluid">
-                    <div className="page-wrapper">
-                      <Routes>
-                        <Route path="*" element={<Unauthorized />} />
-                      </Routes>
+              <><div id="wrapper">
+                    <HeaderComponent />
+                    <div id="content-wrapper" className="d-flex flex-column">
+                      <NavbarComponent />
+                      {/* Main Content */}
+                      <div id="content" className="container-fluid">
+                        <div className="page-wrapper">
+                          <Routes>
+                            <Route path="*" element={<Unauthorized />} />
+                          </Routes>
+                        </div>
+                      </div>
+                      {/* End of Content Wrapper */}
+                      <FooterComponent />
                     </div>
-                  </div>
-                  {/* End of Content Wrapper */}
-                  <FooterComponent />
-                </div>
-              </div>
+                  </div><a className="scroll-to-top rounded" href="#page-top">
+                      <i className="fas fa-angle-up"></i>
+                    </a><div
+                      className="modal fade"
+                      id="logoutModal"
+                      tabIndex="-1"
+                      role="dialog"
+                      aria-labelledby="exampleModalLabel"
+                      aria-hidden="true"
+                    >
+                      <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                          <div className="modal-header">
+                            <h5
+                              className="modal-title"
+                              id="exampleModalLabel"
+                              style={{ fontWeight: "bold" }}
+                            >
+                              Keluar
+                            </h5>
+                            <button
+                              className="close"
+                              type="button"
+                              data-dismiss="modal"
+                              aria-label="Close"
+                            >
+                              <span aria-hidden="true">×</span>
+                            </button>
+                          </div>
+                          <div className="modal-body">
+                            Pilih Keluar Untuk Keluar Dari Program.
+                          </div>
+                          <div className="modal-footer">
+                            <button
+                              className="btn btn-secondary"
+                              type="button"
+                              data-dismiss="modal"
+                            >
+                              Batal
+                            </button>
+                            <a className="btn btn-danger font-weight-bold" href="/">
+                              Keluar
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    </div></>
             )}
           </>
         )}
